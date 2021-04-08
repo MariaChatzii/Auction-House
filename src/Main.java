e0d8b8075c59b435a2fe7dde17336ca9323a2579
MariaChatzii <maria2312ch@gmail.com>
2021-04-08 19:46:41 +0300

    Starting with reading from input file

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args){
 
         ArrayList<Item> itemsForSelling = new InputFileReader().readActions();
         for(Item item: itemsForSelling)
