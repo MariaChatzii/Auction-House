84685768fd60f410714e3a4fc6ffb66295471ae4
MariaChatzii <maria2312ch@gmail.com>
2021-04-14 10:46:07 +0300

    Testing setResultData method

--- src/ItemTest.java
+++ src/ItemTest.java
@@ -108,4 +108,30 @@ class ItemTest {
         item1.setBids(bids5);
         assertEquals(100, item1.getMaxBidPrice());
     }
+
+    @Test
+    void setResultData(){
+        Result result1 = item1.getResult();
+        item1.setResultData();
+
+        assertEquals(-1, result1.getWinnerId());
+        assertEquals("UNSOLD", result1.getStatus());
+        assertEquals(0.00, result1.getPricePaid());
+        assertEquals(0, result1.getCloseTime());
+    }
+
+    @Test
+    void setWinnerData(){
+
+    }
+
+    @Test
+    void setNoWinnerData(){
+
+    }
+
+    @Test
+    void setResult(){
+
+    }
 }
